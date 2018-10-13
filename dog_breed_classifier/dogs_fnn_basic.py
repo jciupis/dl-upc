5be8e8ef1d99f66284bc4320209d3ad3bdd5f262
jciupis <jedrzej.ciupis@gmail.com>
2018-10-13 15:32:23 +0200

    Start working with more complex FNNs

--- dog_breed_classifier/dogs_fnn_basic.py
+++ dog_breed_classifier/dogs_fnn_basic.py
@@ -12,7 +12,7 @@ test_set_len = x_test.shape[0]
 input_len = x_train.shape[1] * x_train.shape[2] * x_train.shape[3]
 nb_breeds = y_train.shape[1]
 batch_size = 128
-nb_epochs = 20
+nb_epochs = 10
 
 # Reshape the data to make it suitable for a flattened input layer
 x_train = x_train.reshape(train_set_len, input_len)
@@ -20,9 +20,9 @@ x_test = x_test.reshape(test_set_len, input_len)
 
 # Define the NN architecture
 nn = Sequential()
-nn.add(Dense(128, activation='relu', input_dim=input_len))
-nn.add(Dense(256, activation='relu'))
-nn.add(Dense(64, activation='relu'))
+nn.add(Dense(256, activation='relu', input_dim=input_len))
+# nn.add(Dense(256, activation='relu'))
+# nn.add(Dense(64, activation='relu'))
 nn.add(Dense(nb_breeds, activation='softmax'))
 
 # Compile the NN
@@ -33,13 +33,13 @@ history = nn.fit(x_train, y_train, batch_size=batch_size, epochs=nb_epochs, vali
 
 # Evaluate the model with test set
 score = nn.evaluate(x_test, y_test, verbose=0)
-print('test loss:', score[0])
-print('test accuracy:', score[1])
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
 
 # Plots
 plt.plot_accuracy(history)
 plt.plot_loss(history)
 
 # Confusion matrix and classification report
-plt.clf_statistics(nn, x_test, y_test)
+plt.get_statistics(nn, x_test, y_test)
 
