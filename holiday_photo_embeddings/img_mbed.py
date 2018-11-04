d1926b36f6b3f86dcf996ff60ede5aa31386d665
jciupis <jedrzej.ciupis@gmail.com>
2018-11-04 19:19:27 +0100

    Add filename labels to embeddings

--- holiday_photo_embeddings/img_mbed.py
+++ holiday_photo_embeddings/img_mbed.py
@@ -24,6 +24,7 @@ def input_pipeline(img_paths, batch_size):
     # Iterate over all image paths
     for n in range(len(img_paths)):
         x_batch = np.zeros((0, IMG_SIZE, IMG_SIZE, 3))
+        y_batch = []
         for i in range(batch_size):
             try:
                 img_path = img_paths.pop(0)
@@ -34,9 +35,11 @@ def input_pipeline(img_paths, batch_size):
             x = image.img_to_array(img)
             x = np.expand_dims(x, axis=0)
             x_batch = np.concatenate((x_batch, x), axis=0)
+            y_batch.append(img_path)
 
         if x_batch.shape[0] != 0:
-            yield x_batch
+            yield x_batch, y_batch
+
 
 def generate_embeddings():
     """
@@ -50,9 +53,8 @@ def generate_embeddings():
 
     # Create variables for embeddings generation.
     embeddings = np.zeros((0, EMBEDDING_SIZE))
-
+    labels = []
     images = [os.path.join(IMG_PATH, f) for f in os.listdir(IMG_PATH)]
-
     step = 0
 
     nb_steps = int(len(images) / BATCH_SIZE)
@@ -60,17 +62,18 @@ def generate_embeddings():
         nb_steps += 1
 
     # Generate embeddings.
-    for batch in input_pipeline(images, BATCH_SIZE):
-        x = preprocess_input(batch)
+    for x_batch, y_batch in input_pipeline(images, BATCH_SIZE):
+        x = preprocess_input(x_batch)
         batch_embeddings = model.predict(x)
         embeddings = np.concatenate((embeddings, batch_embeddings), axis=0)
+        labels.extend(y_batch)
 
         print('Progress: ' + str((step / nb_steps) * 100) + '%')
         step += 1
 
     # Save embeddings to a file.
     print('Saving dataset embeddings into img_mbed.npz file...')
-    np.savez('imb_mbed.npz', embeddings=embeddings)
+    np.savez('img_mbed.npz', embeddings=embeddings, labels=labels)
 
 
 def main():
